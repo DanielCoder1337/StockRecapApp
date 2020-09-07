2e37e3e4dc7371e0bd5e7dfad7629472a391c74f
OskarEek <farssan123@hotmail.se>
2020-09-07 15:56:03 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -37,6 +37,11 @@ app.use(cookieParser());
 //csrf
 app.use(csrf({ cookie: true }));
 
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
 app.get("/", function (request, response) {
     response.render("home.hbs")
 })
