155744b37473ff106bbb0b1ba793ce5b389e7b41
Oskar Eek <farssan123@hotmail.se>
2020-09-07 15:49:38 +0200

    initial commit

---
+++ myApp.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const bodyParser = require("body-parser");
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+//Initialize handlebars
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+app.set('views', './views/');
+app.set('view engine', 'hbs');
+
+//static folders
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/js'));
+
+//bodyparser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+
+//cookieparser
+app.use(cookieParser());
+
+//csrf
+app.use(csrf({ cookie: true }));
+
+app.get("/", function (request, response) {
+    response.render("home.hbs")
+})
+
+
+app.listen(PORT, HOST);
